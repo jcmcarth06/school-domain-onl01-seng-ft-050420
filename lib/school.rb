fca88bd1c5446b491bb07b7281a05e87e3be0100
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-13 21:02:06 +0000

    Automatically backed up by Learn

---
+++ lib/school.rb
@@ -0,0 +1,19 @@
+class School
+  def initialize(name)
+    @name = name
+    roster = {}
+  end 
+
+  def roster
+    @roster
+  end
+
+  def add_students
+  
+  end
+
+  def grade
+    
+  end
+end 
+
